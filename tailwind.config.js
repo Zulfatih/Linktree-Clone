a6ced84a88ebe9848e5cc842a51f853f714fd8e8
Muhammad Zulfatih <zulfatihfreelance@gmail.com>
2023-04-28 16:18:19 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "ocean-blue": "#00a1ff",
+        "ocean-green": "#00ff8f",
+      },
+    },
+  },
+  plugins: [],
+};
